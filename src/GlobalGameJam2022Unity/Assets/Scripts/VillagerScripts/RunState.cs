b1f3923ed93898826a89054b22fd47837bebc002
Caleb Phillips <53085992+Lebyfochs@users.noreply.github.com>
2022-01-29 20:43:50 +0000

    Working run state.
    Villager now runs from desired object (Transform).

--- src/GlobalGameJam2022Unity/Assets/Scripts/VillagerScripts/RunState.cs
+++ src/GlobalGameJam2022Unity/Assets/Scripts/VillagerScripts/RunState.cs
@@ -5,18 +5,37 @@ public class RunState : MonoBehaviour, IState
 {
     VillagerAgent villager;
 
-    private void Start()
+
+
+    public Transform runFromObj;
+
+    private void Awake()
     {
         villager = GetComponent<VillagerAgent>();
+        
+
+        
     }
 
 
+    //Uses magnitude to calculate distance between villager and player.
     public IEnumerator DoState()
     {
-        
+        Vector3 runAway;
 
-        villager.transform.position += new Vector3(2.0f, 0.0f, 0.0f) * Time.deltaTime; //Used for testing purposes.
-        yield return null;
+        while(true)
+        {
+            
+            runAway = (transform.position - runFromObj.position).normalized;
+            villager.navAI.destination = transform.position + runAway * 5;
+            yield return null;
+        }
+      
+        
+        //If within range move away quickly.
+
+       // villager.transform.position += new Vector3(2.0f, 0.0f, 0.0f) * Time.deltaTime; //Used for testing purposes.
+        
     }
 
         
